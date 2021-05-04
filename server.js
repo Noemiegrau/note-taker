ab50f2e65c2e312144d5d915ef880e42e73fc438
Noemiegrau <noemiegrau@outlook.fr>
2021-05-04 11:54:26 -0700

    test heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -17,12 +17,12 @@ app.use((req, res) => {
 });
 
 function filterByQuery(query, notesArray) {
-    let filteredResults = notesArray;
-    if (query.title) {
-    filteredResults = filteredResults.filter(note => note.title === query.title);
-    }
-    return filteredResults;
-};
+  let filteredResults = notesArray;
+  if (query.title) {
+      filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  return filteredResults;
+}
 
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
@@ -57,40 +57,40 @@ function deleteNote(id, notesArray) {
 
 // GET /notes should return the notes.html file
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // GET note title query
 app.get('/api/notes', (req, res) => {
-    let results = notes;
+  let results = notes;
   if (req.query) {
-    results = filterByQuery(req.query, results);
+      results = filterByQuery(req.query, results);
   }
   res.json(results);
 });
 
 // GET id query
-app.get('api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
     let results = findById(req.params.id, notes);
     res.json(results);
 });
 
 // Create note
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
     const note = createNote(req.body, notes);
     res.json(note);
 });
 
 // Delete note
-app.delete('api/notes', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     let results = deleteNote(req.params.id, notes);
     res.json(results);
 });
 
 // GET /* should return the index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
