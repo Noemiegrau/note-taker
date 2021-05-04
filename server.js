80f0238934ed7763d0cd35e2e8e2547139c9078a
Noemiegrau <noemiegrau@outlook.fr>
2021-05-04 10:43:41 -0700

    added path module to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { notes } = require('./db/db.json');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
